6b89ef9bd441713a1e7fe82095d696ce421d9def
Andrew Chin <correctbutnotright@outlook.com>
2021-08-26 23:01:09 -0700

    Added components for modularity

--- frontend/components/manga-description.tsx
+++ frontend/components/manga-description.tsx
@@ -1,20 +1,24 @@
 import React, { useState } from 'react'
+import jsonFetch from '~/lib/fetch.ts'
 
-/* TODO:
- * - understand function parameters
- * - JSON check for English description */
 export default function MangaDescription({ uuid }: { uuid: string }) {
+  // GET request URL
+  let url = 'https://api.mangadex.org/manga/' + uuid;
+
+  // Init a React state for 'description' and define how to modify it
   const [description, setDescription] = useState("");
-  fetch('https://api.mangadex.org/manga/' + uuid)
-    .then(response => response.json())
-    .then(jsonData => {
-      setDescription(jsonData.data.attributes.description.en);
-    })
-    .catch(err => {
-      console.log(err);
-      setDescription("Description not found");
-    });
-  
+  let jsonClosure = (jsonData) => {
+    if(jsonData.data.attributes.description.hasOwnProperty('en')) {
+      setDescription(jsonData.data.attributes.description['en']);
+    }
+    else {
+      setDescription("Description not found.");
+    }
+  };
+
+  // JS fetch
+  jsonFetch(url, jsonClosure);
+
   return (
     <p>{description}</p>
   )
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,37 +1,19 @@
-import { useDeno } from 'aleph/react'
 import React from 'react'
-import Logo from '~/components/logo.tsx'
-import useCounter from '~/lib/useCounter.ts'
-import Banner from 'react-banner'
-import '~/style/react-banner.css'
-import MangaDescription from '~/components/manga-description.tsx'
+import MangaBanner from '~/components/banner.tsx'
 
 export default function Home() {
-  const [count, isSyncing, increase, decrease] = useCounter()
-  const version = useDeno(() => Deno.version.deno)
-
   return (
     <div className="page">
       <head>
-        <title>Hello World - Aleph.js</title>
+        <title>Hello World - Mangadex Front</title>
         {/* <link rel="stylesheet" href="../style/index.css" /> */}
       </head>
 
-      <Banner
-        logo="My Logo"
-        url={ window.location.pathname }
-        items={[
-          { "content": "Example Link", "url": "/example" },
-          { "content": "Another", "url": "/another" },
-          { "content": "Link w/ Children", "url": "/children", "children": [
-            { "content": "John", "url": "/children/john" },
-            { "content": "Jill", "url": "/children/jill" },
-            { "content": "Jack", "url": "/children/jack" }
-          ]}
-        ]} />
+      <MangaBanner/>
+
       <div className="App">
         <h1>Hello World!</h1>
-        <MangaDescription uuid="a96676e5-8ae2-425e-b549-7f15dd34a6d8" />
+        <a href="/manga">TEMPORARY LINK TO MANGA PAGE</a>
       </div>
     </div>
   )
