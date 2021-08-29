2aeb19d8b557ab0e6153172e3b8229cb1f5c944e
Andrew Chin <correctbutnotright@outlook.com>
2021-08-28 21:39:04 -0700

    Cleaned up MangaCover code

--- frontend/components/manga-cover.tsx
+++ frontend/components/manga-cover.tsx
@@ -5,41 +5,21 @@ import { FETCH_LIMIT, COVER_BASE_URL } from '~/lib/constants.ts'
 async function getCover(uuid: string) {
   let response = await fetch('https://api.mangadex.org/cover?manga[]=' + uuid);
   let jsonData = await response.json();
-  /* .then(jsonData => parseInt(jsonData.total, 10)); */
   const total = parseInt(jsonData.total, 10);
   let tempVolume = [-1, "getCover"];
-  // let [tempVolume, setTempVolume] = useState([-1, "getCover"]);
   
-  //XXX block seems to work, expand on this
-  // response = await fetch('https://api.mangadex.org/cover?limit=' + FETCH_LIMIT + '&manga[]=' + uuid);
-  // jsonData = await response.json();
-  // // parseInt(jsonData.results[0].data.attributes.volume, 10);
-  // tempVolume[0] = parseInt(jsonData.results[0].data.attributes.volume, 10);
-  // tempVolume[1] = jsonData.results[0].data.attributes.fileName;
-
-  // let vol = parseInt(jsonData.results[0].data.attributes.volume, 10);
-  // let filename = jsonData.results[0].data.attributes.fileName;
-  // setTempVolume([vol, filename]);
-
   for(let i=0; i<total; i+=FETCH_LIMIT) { // Due to API fetch limits
     const response = await fetch('https://api.mangadex.org/cover?limit=' + FETCH_LIMIT + '&manga[]=' + uuid);
-    /* .then(response => response.json()) */
     const jsonData = await response.json();
-    // tempVolume[0] = parseInt(jsonData.results[0].data.attributes.volume, 10);
-    // tempVolume[1] = jsonData.results[0].data.attributes.fileName;
 
-    //TODO make this work!
     for(let j=0; j<jsonData.results.length; j++) { // Iterate over all results returned from our fetch
       if(jsonData.results[j].data.attributes.volume !== null) {
         if(parseInt(jsonData.results[j].data.attributes.volume, 10) > tempVolume[0]) {
-          /* setTempVolume([parseInt(jsonData.results[j].data.attributes.volume, 10), jsonData.results[j].data.attributes.fileName]); */
-          /* tempVolume = [parseInt(jsonData.results[j].data.attributes.volume, 10), jsonData.results[j].data.attributes.fileName]; */
           tempVolume[0] = parseInt(jsonData.results[j].data.attributes.volume, 10);
           tempVolume[1] = jsonData.results[j].data.attributes.fileName;
         }
       }
       else if(tempVolume[0] === -1) {
-        /* setTempVolume([0, jsonData.results[j].data.attributes.fileName]); */
         tempVolume = [0, jsonData.results[j].data.attributes.fileName];
       }
     }
@@ -47,15 +27,10 @@ async function getCover(uuid: string) {
   return tempVolume[1];
 }
 
-//TODO figure out how to loop async data
 // MangaCover() fetches the latest cover for the given manga uuid
 // Inputs
 // - uuid: valid manga uuid
 export default function MangaCover({ uuid }: { uuid: string }) {
-  /* const [volumePair, setVolumePair] = useState([-1, "MangaCover"]); */
-  /* const [tempVolume, setTempVolume] = useState([-1, ""]); */
-  /* const [counter, setCounter] = useState(0);
-   * const [total, setTotal] = useState(0); */
   const [fileurl, setFileurl] = useState("");
 
 
@@ -68,71 +43,6 @@ export default function MangaCover({ uuid }: { uuid: string }) {
     });
   
 
-  // fetch('https://api.mangadex.org/cover?manga[]=' + uuid)
-  //   .then(response => response.json())
-  //   .then(jsonData => { // Fetch #1 to find out total results
-  //     /* setTotal(parseInt(jsonData.total, 10));
-  //      * setCounter(parseInt(jsonData.offset, 10) + parseInt(jsonData.limit, 10)); */
-  //     /* console.log(jsonData['offset'] + ' ' + jsonData.limit + ' ' + jsonData.total); */
-  //     /* console.log(jsonData); */
-  //     /* setVolumePair([0, "yahallo"]); */
-  //     /* return [parseInt(jsonData.offset, 10) + parseInt(jsonData.limit, 10), parseInt(jsonData.total, 10)]; */
-  //     return parseInt(jsonData.total, 10);
-  //   })
-  //   .then(total => { // Fetch #2 loop on all of the results
-  //     for(let i=0; i<total; i+=FETCH_LIMIT) {
-  //       console.log(i);
-  //       /* setTempVolume([i, "test"]); */
-  //       fetch('https://api.mangadex.org/cover?limit=' + FETCH_LIMIT + '&manga[]=' + uuid)
-  //         .then(response => response.json())
-  //         .then(jsonData => {
-  //           /* setTempVolume([0, jsonData.results.length]); */
-  //           //FIXME prone to issues if volume isn't defined
-  //           /* setTempVolume(jsonData.results.length); */
-  //           // for(let obj in jsonData.results) {
-  //           //   if(obj.data.attributes.volume !== null) {
-  //           //     if(parseInt(obj.data.attributes.volume, 10) > tempVolume[0]) {
-  //           //       setTempVolume([parseInt(obj.data.attributes.volume, 10), obj.data.attributes.fileName]);
-  //           //     }
-  //           //   }
-  //           //   else if(tempVolume[0] === -1) {
-  //           //     setTempVolume([0, obj.data.attributes.fileName]);
-  //           //   }
-  //           // }
-  //           for(let j=0; j<jsonData.results.length; j++) {
-  //             if(jsonData.results[j].data.attributes.volume !== null) {
-  //               if(parseInt(jsonData.results[j].data.attributes.volume, 10) > tempVolume[0]) {
-  //                 setTempVolume([parseInt(jsonData.results[j].data.attributes.volume, 10), jsonData.results[j].data.attributes.fileName]);
-  //               }
-  //             }
-  //             else if(tempVolume[0] === -1) {
-  //               setTempVolume([0, jsonData.results[j].data.attributes.fileName]);
-  //             }
-  //           }
-  //           // jsonData.results.forEach((obj) => {
-  //           //   if(obj.data.attributes.volume !== null) {
-  //           //     if(parseInt(obj.data.attributes.volume, 10) > tempVolume[0]) {
-  //           //       setTempVolume([parseInt(obj.data.attributes.volume, 10), obj.data.attributes.fileName]);
-  //           //     }
-  //           //   }
-  //           //   else if(tempVolume[0] === -1) {
-  //           //     setTempVolume([0, obj.data.attributes.fileName]);
-  //           //   }
-  //           // });
-  //         })
-  //         .catch(err => console.error(err));
-  //     }
-  //     setVolumePair(tempVolume);
-  //   })
-  //   .catch(e => {
-  //     setTimeout(() => {
-  //       if(content === "") {
-  //         setVolumePair([-1, "Cover not found"]);
-  //       }
-  //     }, ERROR_TIMEOUT);
-  //     console.error(e);
-  //   });
-  
   return (
     <img src={fileurl}/>
   )
